b0ec4ba0d22cfb3d506ae182a3dfb336fa6b1539
AnnaTarnavska <anja.t0410@gmail.com>
2025-07-11 20:30:33 +0300

    fix joi register val

--- src/validation/register-validation-schema.js
+++ src/validation/register-validation-schema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const registerUserValidationSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().regex().min(6).required(),
+    password: Joi.string().min(6).required(),
 
 });
